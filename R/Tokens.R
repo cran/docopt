daefb2891939c84aba13a78c6644b7af3efb7cac
Edwin de Jonge <edwindjonge@gmail.com>
2014-06-07 00:00:00 +0000

    version 0.1.1

---
+++ R/Tokens.R
@@ -0,0 +1,91 @@
+Tokens <- setRefClass( "Tokens"
+   , fields=list( tokens="character"
+                , error="function"
+                , strict="logical"
+                , defining=function(){!strict}
+                )
+   , methods=list(
+     initialize = function(tokens=character(), error=stop){
+       .tokens <- gsub("^\\s+|\\s+$", "", tokens)
+       args <- str_extract_all(.tokens, "<.*?>")[[1]]
+       args_s <- gsub("\\s", "____", args)
+       for (i in seq_along(args)){
+          .tokens <- gsub(args[i], args_s[i], .tokens, fixed = T)
+       }
+       .tokens <- strsplit(.tokens, "\\s+")[[1]]
+       tokens <<- gsub("____", " ", .tokens, fixed=T)
+       if (missing(error)){
+         strict <<- TRUE
+       } else {
+         strict <<- FALSE
+       }
+       error <<- error
+     },
+     current = function(){
+       if (length(tokens)){
+         tokens[1]
+        } else {
+          ""
+        }
+     },
+     shift = function(){
+       h <- head(tokens, 1)
+       tokens <<- tail(tokens, -1)
+       if (length(h)) h else ""
+     },
+     move = function(){
+       h <- head(tokens, 1)
+       tokens <<- tail(tokens, -1)
+       if (length(h)) h else ""
+     },
+     #     toString: -> ([].slice.apply @).toString()
+     toString = function(){
+       tokens
+     },
+     show = function(){
+       cat("Tokens:", toString())
+     },
+     # 
+     #     join: (glue) -> ([].join.apply @, glue)
+     join = function(glue){
+       paste0(tokens, collapse=glue)
+     }
+  )
+)
+
+#' index tokens
+#' 
+#' index tokens
+#' @keywords internal
+setMethod("[",
+          signature(x = "Tokens"),
+          function (x, i, drop = TRUE) 
+          {
+            x$tokens[i]
+          }
+)
+
+#' index tokens
+#' 
+#' index tokens
+#' @keywords internal
+setMethod("[<-",
+          signature(x = "Tokens"),
+          function (x, i, value) 
+          {
+            x$tokens[i] <- value
+          }
+)
+
+#' to character
+#' 
+#' to character
+#' @keywords internal
+setMethod("as.character", signature(x="Tokens"), function(x, ...){x$tokens})
+
+
+# # testing 1, 2, 3
+# t <- Tokens(c("a", "b"))
+# t$current()
+# t$shift()
+# t
---
+++ README.md
@@ -0,0 +1,76 @@
+[![Build Status](https://travis-ci.org/edwindj/docopt.R.svg?branch=master)](https://travis-ci.org/edwindj/docopt.R)
+
+docopt
+========================================================
+
+`docopt` helps you to:
+
+- define an interface for your command-line app, and
+- automatically generate a parser for it.
+
+For more information see [docopt.org](http://docopt.org)
+
+R package `docopt` is an implementation of [docopt](http://docopt.org) in the R language.
+
+Install
+-------
+
+The latest version of `docopt` can be installed using:
+
+```S
+library(devtools)  # make sure to have devtools 1.4!
+install_github("edwindj/docopt.R")
+```
+
+
+It is tested against the tests defined for the reference implementation.
+It passes most tests. It currently fails tests that 
+
+- count arguments: `prog -v -v` should return `list(v=2)`
+- multiple prog statement: i.e. `prog -a \n prog -b`
+
+The tests can be run using devtools `test()` and can be found in "inst/tests"
+
+Usage
+-----
+
+docopt uses the description of the commandline interface to parse command line
+arguments.
+
+
+```S
+'usage: prog [-a -r -m <msg>]
+
+options:
+ -a        Add
+ -r        Remote
+ -m <msg>  Message' -> doc
+
+# load the docopt library
+library(docopt)
+# retrieve the command line arguments
+opts <- docopt(doc)
+# what are the options?
+str(opts)
+```
+
+```
+## List of 3
+##  $ -a: logi FALSE
+##  $ -r: logi FALSE
+##  $ -m: chr "<msg>"
+```
+
+```S
+
+# or set them manually
+opts <- docopt(doc, "-m Hello")
+str(opts)
+```
+
+```
+## List of 3
+##  $ -a: logi FALSE
+##  $ -r: logi FALSE
+##  $ -m: chr "Hello"
+```
